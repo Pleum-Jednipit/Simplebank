4b81163664eb64d0c3dd9de64fb9666d44845698
Pleum-Jednipit <jednitan10@gmail.com>
2022-07-20 01:46:41 +0700

    Implement account api and add config file

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,20 +6,19 @@ import (
 	"os"
 	"testing"
 
+	"github.com/Pleum-Jednipit/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5000/simple_bank?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("Cannot load config:", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
