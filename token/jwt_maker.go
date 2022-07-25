c2ec80aad19a5e33376e449ea3d0f9715745a120
Pleum-Jednipit <jednitan10@gmail.com>
2022-07-25 17:11:17 +0700

    Implement creating jwt and paseto token

---
+++ token/jwt_maker.go
@@ -0,0 +1,59 @@
+package token
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+const minSecretKeySize = 32
+
+// JWTMAKER is a JSON Web Token maker
+type JWTMaker struct {
+	sercretKey string
+}
+
+// NewJWTMaker create a new JWTMaker
+func NewJWTMaker(sercretKey string) (Maker, error) {
+	if len(sercretKey) < minSecretKeySize {
+		return nil, fmt.Errorf("Invalod key size: must be at least %d character", minSecretKeySize)
+	}
+	return &JWTMaker{sercretKey}, nil
+}
+
+// CreateToken creates a new token for a specific username and duration
+func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	return jwtToken.SignedString([]byte(maker.sercretKey))
+}
+
+// VerifyToken checks if the token is valid or not
+func (maker *JWTMaker) VerifyToken(token string) (*Payload, error) {
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, ErrInvalidToken
+		}
+		return []byte(maker.sercretKey), nil
+	}
+	jwtToken, err := jwt.ParseWithClaims(token, &Payload{}, keyFunc)
+	if err != nil {
+		verr, ok := err.(*jwt.ValidationError)
+		if ok && errors.Is(verr.Inner, ErrExpiredToken) {
+			return nil, ErrExpiredToken
+		}
+		return nil, ErrInvalidToken
+	}
+	payload, ok := jwtToken.Claims.(*Payload)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+	return payload, nil
+}
