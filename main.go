1dae6202c3a5b48d72152318d8d36e7d113a3ecd
Pleum-Jednipit <jednitan10@gmail.com>
2022-08-06 16:56:58 +0700

    Change to gin server

--- main.go
+++ main.go
@@ -29,8 +29,9 @@ func main() {
 		log.Fatal("Cannot connect to db:", err)
 	}
 	store := db.NewStore(conn)
-	go runGatewayServer(config, store)
-	runGrpcServer(config, store)
+	// go runGatewayServer(config, store)
+	// runGrpcServer(config, store)
+	runGinServer(config, store)
 }
 
 func runGrpcServer(config util.Config, store db.Store) {
